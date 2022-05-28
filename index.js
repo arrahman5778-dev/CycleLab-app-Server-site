59d2b1e8f34916343470c2cc820d078c0518f946
arrahman5778 <arrahman5778@gmail.com>
2022-05-29 00:32:39 +0600

    MongoDB Username-pass added

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const jwt = require("jsonwebtoken");
 app.use(cors());
 app.use(express.json());
 
-
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.attrdyd.mongodb.net/?retryWrites=true&w=majority;`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
